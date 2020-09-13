d7a7c9fb75ad370b8d70b63a40e44dd92af3130e
roleyfoley <roleyfoley@gmail.com>
2020-09-13 08:32:34 +0000

    refactor: test genertion using management contract

--- test/run_aws_template_tests.sh
+++ test/run_aws_template_tests.sh
@@ -4,7 +4,8 @@ echo "###############################################"
 echo "# Running template tests for the AWS provider #"
 echo "###############################################"
 
-TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-"./hamlet_tests"}"
+DEFAULT_TEST_OUTPUT_DIR="$(pwd)/hamlet_tests"
+TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-${DEFAULT_TEST_OUTPUT_DIR}}"
 
 if [[ -d "${TEST_OUTPUT_DIR}" ]]; then
     rm -r "${TEST_OUTPUT_DIR}"
@@ -13,20 +14,29 @@ else
     mkdir -p "${TEST_OUTPUT_DIR}"
 fi
 
-echo "Output Dir: ${TEST_OUTPUT_DIR}"
-echo "Generating unit list..."
-${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l unitlist
-UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < "${TEST_OUTPUT_DIR}/unitlistconfig.json")"
+echo " -Output Dir: ${TEST_OUTPUT_DIR}"
+echo ""
+echo "--- Generating Management Contract ---"
+echo ""
 
-for unit in $UNIT_LIST; do
-    echo "Creating templates for $unit ..."
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l segment -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l solution -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l application -u $unit > /dev/null 2>&1 || true
+${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l unitlist
+UNIT_LIST=`jq -r '.Stages[].Steps[].Parameters | "-l \(.DeploymentGroup) -u \(.DeploymentUnit)"' < ${TEST_OUTPUT_DIR}/unitlist-managementcontract.json`
+readarray -t UNIT_LIST <<< "${UNIT_LIST}"
+
+for unit in "${UNIT_LIST[@]}";  do
+    echo ""
+    echo "--- Generating $unit ---"
+    echo ""
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -x ${unit} || exit $?
 done
+
+echo ""
+echo "--- Running Tests ---"
+echo ""
 
 hamlet test generate --directory "${TEST_OUTPUT_DIR}" -o "${TEST_OUTPUT_DIR}/test_templates.py"
 
+pushd $(pwd)
 cd "${TEST_OUTPUT_DIR}"
-echo "Running Tests..."
 hamlet test run -t "./test_templates.py"
+popd
